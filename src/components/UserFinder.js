c6c55410f97e88feabb9174873c1baca636f6599
Dhruv Rana <dhruvrana0000@gmail.com>
2022-06-16 23:33:54 +0530

    Creation and Use of ErrorBoundary component to handle errors using componentDidCatch method

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,6 +1,7 @@
 import { Fragment, Component } from "react";
 import classes from "./UserFinder.module.css";
 import Users from "./Users";
+import ErrorBoundary from "./ErrorBoundary";
 
 const DUMMY_USERS = [
   { id: "u1", name: "Max" },
@@ -52,7 +53,9 @@ class UserFinder extends Component {
         <div className={classes.finder}>
           <input type="search" onChange={this.searchChangeHandler.bind(this)} />
         </div>
-        <Users users={this.state.filteredUsers} />
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
       </Fragment>
     );
   }
